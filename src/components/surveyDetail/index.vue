27d8ff6204d9b113ace49be1b542df4e65c7bc8d
胡敬洋 <a992016805b@163.com>
2019-10-12 17:44:34 +0800

    问卷调查详情页面构建

--- src/components/surveyDetail/index.vue
+++ src/components/surveyDetail/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="survey-detail">
     <div v-for="(item,index) in data.questions" :key="index">
-      <h2>{{index}}</h2>
+      <span>{{data.questions.length}}</span>
+
+      <div class="question-item">
+        <div v-if="item.type === 'radio'">
+          <div class="select-control-group">
+            <mu-flex class="select-control-row" v-for="element in item.content" :key="element.id">
+              <mu-radio :value="element.id" v-model="radio.value1" :label="element.text"></mu-radio>
+            </mu-flex>
+          </div>
+        </div>
+        <div v-else-if="item.type === 'multiple'">multiple</div>
+        <div v-else-if="item.type === 'shortAnswer'">shortAnswer</div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,13 +23,18 @@ import api from "api/api-handler";
 export default {
   data() {
     return {
-      data: {}
+      data: {},
+      radio: {
+        value1: [],
+        value2: "heart",
+        value3: "disable"
+      }
     };
   },
   methods: {},
   mounted: function() {
     //保存用户id到 状态管理器 ， 当页面刷新时 自动清除
-    console.log(this.$store.state.userInfo);
+    // console.log(this.$store.state.userInfo);
     //保存用户id到sessionStorage ， 当用关闭项目后 自动清除
     console.log(sessionStorage.getItem("userId"));
   },
@@ -37,5 +54,11 @@ export default {
   width: 100vw;
   min-height: 100vh;
   background-color: #f6f5fa;
+}
+.select-control-row {
+  padding: 8px 0;
+}
+.select-control-group {
+  margin: 16px 0;
 }
 </style>
