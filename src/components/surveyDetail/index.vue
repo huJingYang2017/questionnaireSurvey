968c2ffa1514ffefac94901ebeca9fc327d6945f
胡敬洋 <a992016805b@163.com>
2019-10-14 15:19:54 +0800

    编辑问卷调查详情页

--- src/components/surveyDetail/index.vue
+++ src/components/surveyDetail/index.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="survey-detail">
-    <div v-for="(item,index) in data.questions" :key="index">
-      <span>{{data.questions.length}}</span>
-
+    <div class="question-survey" v-for="(item,index) in data.questions" :key="index">
+      <!-- <span>{{data.questions.length}}</span> -->
+      <div v-if="index === 0">
+        <div class="questionnaire-title">{{data.title}}</div>
+        <div class="questionnaire-dateTime">{{data.dateTime}}</div>
+      </div>
       <div class="question-item">
-        <div v-if="item.type === 'radio'">
-          <div class="select-control-group">
-            <mu-flex class="select-control-row" v-for="element in item.content" :key="element.id">
-              <mu-radio :value="element.id" v-model="radio.value1" :label="element.text"></mu-radio>
-            </mu-flex>
+        <mu-container>
+          <div v-if="item.type === 'radio'">
+            <div class="question-title">
+              <span class="question-required">{{item.required ? '&nbsp;*' : '&nbsp;&nbsp;' }}</span>
+              {{index + 1}}、{{item.question}}
+            </div>
+            <div class="select-control-group">
+              <mu-flex class="select-control-row" v-for="element in item.content" :key="element.id">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <mu-radio
+                  :value="element.id"
+                  v-model="radio.value1"
+                  :label="element.text"
+                ></mu-radio>
+              </mu-flex>
+            </div>
           </div>
+          <div v-else-if="item.type === 'multiple'">
+            <div class="question-title">
+              <span class="question-required">{{item.required ? '&nbsp;*' : '&nbsp;&nbsp;' }}</span>
+              {{index + 1}}、{{item.question}}
+            </div>
+            <div class="select-control-group">
+              <!-- Selects: {{checkbox.value1}} -->
+              <!-- <mu-flex class="select-control-row">
+              <mu-checkbox
+                label="全选"
+                :input-value="checkAll"
+                @change="handleCheckAll"
+                :checked-icon="this.checkbox.value1.length < 3 ? 'indeterminate_check_box' : undefined"
+              ></mu-checkbox>
+              </mu-flex>-->
+              <mu-flex class="select-control-row" v-for="element in item.content" :key="element.id">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <mu-checkbox :value="element.id" v-model="checkbox.value1" :label="element.text"></mu-checkbox>
+              </mu-flex>
+            </div>
+          </div>
+          <div v-else-if="item.type === 'shortAnswer'">
+            <div class="question-title">
+              <span class="question-required">{{item.required ? '&nbsp;*' : '&nbsp;&nbsp;' }}</span>
+              {{index + 1}}、{{item.question}}
+            </div>
+            <mu-text-field
+              class="shortAnswer-textArea"
+              v-model="value9"
+              placeholder="最多可以输入140个文字"
+              multi-line
+              full-width
+              :rows="3"
+              :rows-max="6"
+              :max-length="140"
+              :solo="true"
+            ></mu-text-field>
+            <!-- help-text="sadasd" -->
+          </div>
+        </mu-container>
+      </div>
+
+      <div v-if="index === (data.questions.length -1)">
+        <div>
+          <mu-container class="button-wrapper">
+            <mu-flex justify-content="center" align-items="center">
+              <mu-button full-width color="primary">提交</mu-button>
+            </mu-flex>
+          </mu-container>
         </div>
-        <div v-else-if="item.type === 'multiple'">multiple</div>
-        <div v-else-if="item.type === 'shortAnswer'">shortAnswer</div>
       </div>
     </div>
   </div>
@@ -28,7 +89,13 @@ export default {
         value1: [],
         value2: "heart",
         value3: "disable"
-      }
+      },
+      checkbox: {
+        value1: [],
+        value2: false,
+        value3: false
+      },
+      value9: ""
     };
   },
   methods: {},
@@ -53,12 +120,61 @@ export default {
 .survey-detail {
   width: 100vw;
   min-height: 100vh;
-  background-color: #f6f5fa;
+  background-color: #f4f3f3;
+}
+.question-survey {
+  background-color: #fff;
+  margin-top: 12px;
 }
 .select-control-row {
   padding: 8px 0;
 }
 .select-control-group {
-  margin: 16px 0;
+  // margin-top: 16px;
+  padding-bottom: 8px;
+}
+.button-wrapper {
+  text-align: center;
+  .mu-button {
+    margin: 8px;
+    vertical-align: middle;
+  }
+  a.mu-button {
+    text-decoration: none;
+  }
+}
+.questionnaire-title {
+  text-align: center;
+  font-size: 18px;
+  color: #111;
+  font-weight: bolder;
+  padding: 12px 0 6px 0;
+}
+.questionnaire-dateTime {
+  text-align: center;
+  font-size: 14px;
+  color: #7b7b7b;
+}
+.question-required {
+  color: red;
+}
+.question-title {
+  color: #3b3b3b;
+  font-size: 16px;
+  padding: 12px 0;
+  font-weight: bolder;
+}
+.shortAnswer-textArea {
+  padding: 14px;
+  border: 1px solid #ddd;
+  width: 94%;
+  margin: 14px 3%;
+  font-size: 14px;
+}
+</style>
+
+<style lang="less">
+.shortAnswer-textArea .mu-input-help {
+  padding: 12px 0;
 }
 </style>
